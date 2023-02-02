6da78172c0be69285f329430a646a13c464300db
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-02 14:05:32 +0400

    10.2.3 done

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("Root/files/answer.txt");
-        FileInputStream streaming = new FileInputStream(file);
-        int i = 0;
-
-        while (i != -1) {
+        FileInputStream streaming = new FileInputStream("Root/files/asciitable.txt");
+        int count = 0;
+        int num = 0;
+        while (num != -1) {
             try {
-                i = streaming.read();
+                num = streaming.read();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            System.out.print((char)i);
+            if ('0' <= num && num <= '9') count++;
+        }
+        System.out.println(count);
+        try {
+            streaming.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
+
