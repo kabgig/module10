38c3fee30001eb4d8808fd965348c77b790fbacc
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-02 12:52:16 +0400

    10.2.1 DONE

--- src/OneZeroStream.java
+++ src/OneZeroStream.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class OneZeroStream extends InputStream {
-        int count = 0;
-        byte[] numbers = {1, ' ', 0, ' ', 1, ' ', 0, ' ', 1, ' ', 0, ' ', 1, ' ', 0, ' ', 1, ' ', 0, ' '};
+    int count = 0;
+    byte[] numbers = {'1', ' ', '0', ' '};
 
-    public byte read() throws IOException {
+    public int read() throws IOException {
         if (count == numbers.length) count = 0;
         return numbers[count++];
     }
