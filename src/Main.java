c10b8470532d62935085fbe2f78ffa254d78bed3
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-02 17:50:02 +0400

    ex 10.3.1 DONE

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,15 @@
-import java.io.*;
-import java.time.Duration;
-import java.time.LocalTime;
-
+import java.io.FileOutputStream;
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        copyFileUsingStream("/Users/ansarismagilov/Desktop/1.png", "/Users/ansarismagilov/Desktop/2.png");
-    }
-
-    private static void copyFileUsingStream(String source, String dest) throws IOException {
-        var start = LocalTime.now();
-        InputStream fis = new FileInputStream(source);
-        OutputStream fos = new FileOutputStream(dest);
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = fis.read(buffer)) > 0) {
-            fos.write(buffer, 0, length);
+    public static void main(String[] args) throws IOException{
+        String phrase = "GIVE ME THE CODE, PLEASE";
+        int lng = phrase.length();
+        byte[] letters = new byte[lng];
+        for (int i = 0; i < lng; i++) {
+            letters[i] = (byte)phrase.charAt(i);
         }
-        fis.close();
+        FileOutputStream fos = new FileOutputStream("Root/files/request.txt");
+        fos.write(letters,0,lng);
         fos.close();
-        var end = LocalTime.now();
-        var duration = Duration.between(start, end);
-        System.out.println(duration.toMillis());
     }
 }
