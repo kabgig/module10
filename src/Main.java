c01306031b8f5835e26b43add2649738c262178b
Ansar Ismagilov <ansar89kab@bk.ru>
2023-02-03 16:04:27 +0400

    encoding change theory

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 import java.io.*;
+import java.nio.charset.Charset;
 import java.time.Duration;
 import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        copyFileUsingStream("/Users/ansarismagilov/Desktop/1.png", "/Users/ansarismagilov/Desktop/2.png");
+        copyFileUsingStream(
+                "2.txt",
+                Charset.forName("UTF-8"),
+                "3.txt",
+                Charset.forName("WINDOWS-1251"));
     }
 
-    private static void copyFileUsingStream(String source, String dest) throws IOException {
+    private static void copyFileUsingStream(String source, Charset sEnc, String dest, Charset dEnc) throws IOException {
         var start = LocalTime.now();
-        InputStream fis = new FileInputStream(source);
-        OutputStream fos = new FileOutputStream(dest);
-        byte[] buffer = new byte[1024];
+
+        FileReader fis = new FileReader(source, sEnc);
+        FileWriter fos = new FileWriter(dest, dEnc);
+        char[] buffer = new char[1024];
         int length;
         while ((length = fis.read(buffer)) > 0) {
             fos.write(buffer, 0, length);
